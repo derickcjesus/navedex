a257e8d0801daa091a04d4fad40489e49188d133
Derick de Jesus <derickcjesus@gmail.com>
2021-03-11 11:12:50 -0300

    adiciona AddNavers no App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AddNavers } from './pages/AddNavers';
 import { Login } from './pages/Login';
 import { Navers,  } from './pages/Navers';
 import { GlobalStyle } from './styles/GlobalStyles';
@@ -6,7 +7,7 @@ import { GlobalStyle } from './styles/GlobalStyles';
 function App() {
   return (
     <>
-      <Navers />
+      <AddNavers />
 
       <GlobalStyle />
     </>
