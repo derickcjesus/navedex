e958643c3d3e51142d96ef65ce0d088aec09d034
Derick de Jesus <derickcjesus@gmail.com>
2021-03-13 11:40:39 -0300

    teste de componentes no App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { ModalNaverAtualizado } from './components/ModalNaverAtualizado';
+import { ModalNaverAdicionado } from './components/ModalNaverAdicionado';
+import { ModalNaverExcluido } from './components/ModalNaverExcluido';
 import { ModalExcluirNaver } from './components/ModalExcluirNaver';
+
 import { ModalNaver } from './components/ModalNaver';
 import { AddNavers } from './pages/AddNavers';
 import { EditNavers } from './pages/EditNavers';
 import { Login } from './pages/Login';
-import { Navers,  } from './pages/Navers';
+import { Navers } from './pages/Navers';
 import { GlobalStyle } from './styles/GlobalStyles';
 import { Overlay } from './styles/Overlay';
 
 function App() {
   return (
     <>
-    <Navers />
-      <ModalExcluirNaver />
+    <EditNavers/>
+    <ModalNaver />
 
-      <GlobalStyle />
+    <GlobalStyle />
     </>
   );
 }
